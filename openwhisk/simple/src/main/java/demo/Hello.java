3b0ee935bbda6a4f89622c056a40fc6b2f7fc57f
markfisher <mfisher@pivotal.io>
2017-08-14 17:44:13 -0400

    added simple openwhisk demo

---
+++ openwhisk/simple/src/main/java/demo/Hello.java
@@ -0,0 +1,14 @@
+package demo;
+
+import com.google.gson.JsonObject;
+
+public class Hello {
+
+	public static JsonObject main(JsonObject args) {
+		String name = args.getAsJsonPrimitive("name").getAsString();
+		JsonObject response = new JsonObject();
+		response.addProperty("greeting", "Hello " + name);
+		return response;
+	}
+}
+
